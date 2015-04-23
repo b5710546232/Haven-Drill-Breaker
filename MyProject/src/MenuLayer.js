72f2d3fc6d9a0c8f6c6c2f0a3ebb336288f1d0ec
nattapat <nattapat.su@ku.th>
2015-04-23 21:43:52 +0700

    add how to play button

--- MyProject/src/MenuLayer.js
+++ MyProject/src/MenuLayer.js
@@ -3,25 +3,43 @@ var MenuLayer = cc.LayerColor.extend({
         this._super( new cc.Color( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
         this.bg = new MenuBackGround();
-        this.initComponent();
+        this.createPlayButton();
         this.addChild(this.bg);
         this.scheduleUpdate();
-
-        this.startItem = new cc.MenuItemImage(
-            'res/images/SceneComponent/playBut.png',
-            'res/images/SceneComponent/playBut.png',
-            function () {
-            cc.log('Click');
-            cc.director.runScene(new GamePlayScene() );
-         }, this);
-        this.playButton = new cc.Menu(this.startItem);
-        this.addChild(this.playButton);
-
+        this.initComponent();
         return true;
     },
 
     initComponent:function(){
+    this.createPlayButton();
+    this.createHowToButton();
     },
+
+    createPlayButton:function(){
+      this.playButItem = new cc.MenuItemImage(
+        'res/images/SceneComponent/playBut.png',
+        'res/images/SceneComponent/playBut2.png',
+        function () {
+            cc.director.runScene(new GamePlayScene() );
+        }, this);
+      this.playButton = new cc.Menu(this.playButItem);
+      this.addChild(this.playButton);
+    },
+    createHowToButton:function(){
+      this.howToButItem = new cc.MenuItemImage(
+        'res/images/SceneComponent/howtoPlay_But.png',
+        'res/images/SceneComponent/howtoPlay_But2.png',
+        function () {
+            cc.director.runScene(new GamePlayScene() );
+        }, this);
+      this.howToButton = new cc.Menu(this.howToButItem);
+      this.addChild(this.howToButton);
+      var deltaDistance = -80;
+      this.howToButton.setPosition(screenWidth/2,(screenHeight/2)+deltaDistance);
+    },
+
+
+
     update: function(dt) {
 
     },
